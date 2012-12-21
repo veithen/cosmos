699dd066e2e32035f0b943628ed2c6ec9a525116
Andreas Veithen <andreas.veithen@gmail.com>
2012-12-21 20:29:55 +0100

    First successful download of a Maven dependency from a P2 repository.

--- wagon-p2/src/main/java/com/github/veithen/cosmos/wagon/P2Wagon.java
+++ wagon-p2/src/main/java/com/github/veithen/cosmos/wagon/P2Wagon.java
@@ -1,6 +1,8 @@
 package com.github.veithen.cosmos.wagon;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -10,16 +12,18 @@ import org.apache.maven.wagon.ResourceDoesNotExistException;
 import org.apache.maven.wagon.TransferFailedException;
 import org.apache.maven.wagon.authentication.AuthenticationException;
 import org.apache.maven.wagon.authorization.AuthorizationException;
-import org.apache.maven.wagon.proxy.ProxyInfo;
-import org.apache.maven.wagon.proxy.ProxyInfoProvider;
-import org.eclipse.core.internal.net.ProxyManager;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.p2.core.ProvisionException;
+import org.eclipse.equinox.p2.metadata.IArtifactKey;
+import org.eclipse.equinox.p2.metadata.Version;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactDescriptor;
 import org.eclipse.equinox.p2.repository.artifact.IArtifactRepository;
 
 /**
  * @plexus.component role="org.apache.maven.wagon.Wagon" role-hint="p2"
  *                   instantiation-strategy="per-lookup"
  */
+// TODO: implement StreamingWagon
 public class P2Wagon extends AbstractWagon {
     /**
      * @plexus.requirement
@@ -34,18 +38,17 @@ public class P2Wagon extends AbstractWagon {
 //        ProxyManager.getProxyManager().setProxyData(arg0);
 //        System.out.println(ProxyInfoProvider);
         
-        try {
-            URI p2uri = new URI(repository.getUrl());
-            String ssp = p2uri.getSchemeSpecificPart();
-            int idx = ssp.indexOf('!');
-            if (idx == -1) {
-                throw new ConnectionException("Invalid repository URL");
+        String url = repository.getUrl();
+        if (!url.startsWith("p2:")) {
+            throw new ConnectionException("Invalid repository URL; expected URL with scheme \"p2\"");
+        } else {
+            try {
+                artifactRepository = repoman.loadRepository(new URI(url.substring(3)));
+            } catch (URISyntaxException ex) {
+                throw new ConnectionException("Invalid repository URL", ex);
+            } catch (ProvisionException ex) {
+                throw new ConnectionException("Failed to connect to repository", ex);
             }
-            artifactRepository = repoman.loadRepository(new URI(ssp.substring(0, idx)));
-        } catch (URISyntaxException ex) {
-            throw new ConnectionException("Invalid repository URL", ex);
-        } catch (ProvisionException ex) {
-            throw new ConnectionException("Failed to connect to repository", ex);
         }
     }
 
@@ -55,6 +58,53 @@ public class P2Wagon extends AbstractWagon {
 
     public boolean getIfNewer(String resourceName, File destination, long timestamp) throws TransferFailedException, ResourceDoesNotExistException, AuthorizationException {
         System.out.println(resourceName);
+        
+        // Translate the resource name back into Maven coordinates and file type
+        int fileSlash = resourceName.lastIndexOf('/');
+        if (fileSlash == -1) {
+            throw new ResourceDoesNotExistException(resourceName + " not found");
+        }
+        int versionSlash = resourceName.lastIndexOf('/', fileSlash-1);
+        if (fileSlash == -1) {
+            throw new ResourceDoesNotExistException(resourceName + " not found");
+        }
+        int artifactSlash = resourceName.lastIndexOf('/', versionSlash-1);
+        if (artifactSlash == -1) {
+            throw new ResourceDoesNotExistException(resourceName + " not found");
+        }
+        String groupId = resourceName.substring(0, artifactSlash).replace('/', '.');
+        String artifactId = resourceName.substring(artifactSlash+1, versionSlash);
+        String version = resourceName.substring(versionSlash+1, fileSlash);
+        String file = resourceName.substring(fileSlash+1);
+        if (!file.startsWith(artifactId + "-" + version + ".")) {
+            throw new ResourceDoesNotExistException(resourceName + " not found");
+        }
+        String type = file.substring(artifactId.length() + version.length() + 2);
+        System.out.println("groupId=" + groupId);
+        System.out.println("artifactId=" + artifactId);
+        System.out.println("version=" + version);
+        System.out.println("type=" + type);
+        
+        // Now download the artifact
+        if (type.equals("jar")) {
+            IArtifactKey key = artifactRepository.createArtifactKey(groupId, artifactId, Version.create(version));
+            IArtifactDescriptor[] descriptors = artifactRepository.getArtifactDescriptors(key);
+            IArtifactDescriptor descriptor = descriptors[0];
+            System.out.println(descriptor.getProperties());
+            try {
+                FileOutputStream out = new FileOutputStream(destination);
+                try {
+                    // TODO: process status
+                    artifactRepository.getArtifact(descriptor, out, new NullProgressMonitor());
+                } finally {
+                    out.close();
+                }
+            } catch (IOException ex) {
+                throw new TransferFailedException("Failed to write to " + destination, ex);
+            }
+        } else {
+            throw new ResourceDoesNotExistException(resourceName + " not found");
+        }
         // TODO Auto-generated method stub
         return false;
     }
