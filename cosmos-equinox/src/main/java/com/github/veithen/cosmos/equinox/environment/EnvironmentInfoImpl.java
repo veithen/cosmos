a6becb7acdb8370a2d86ed9a20c8fd26b8b23f6c
Andreas Veithen <andreas.veithen@gmail.com>
2018-03-23 11:58:21 +0000

    Add code to support starting the org.eclipse.core.runtime bundle

---
+++ cosmos-equinox/src/main/java/com/github/veithen/cosmos/equinox/environment/EnvironmentInfoImpl.java
@@ -0,0 +1,81 @@
+/*-
+ * #%L
+ * Cosmos
+ * %%
+ * Copyright (C) 2012 - 2018 Andreas Veithen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.veithen.cosmos.equinox.environment;
+
+import org.eclipse.osgi.service.environment.EnvironmentInfo;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service={EnvironmentInfo.class}, xmlns="http://www.osgi.org/xmlns/scr/v1.1.0")
+public class EnvironmentInfoImpl implements EnvironmentInfo {
+    @Override
+    public String[] getCommandLineArgs() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String[] getFrameworkArgs() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String[] getNonFrameworkArgs() {
+        return null;
+    }
+
+    @Override
+    public String getOSArch() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getNL() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getOS() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getWS() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean inDebugMode() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean inDevelopmentMode() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getProperty(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String setProperty(String key, String value) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ osgi-tests/eclipse-core-runtime-test/src/test/java/EclipseCoreRuntimeTest.java
@@ -0,0 +1,36 @@
+/*-
+ * #%L
+ * Cosmos
+ * %%
+ * Copyright (C) 2012 - 2018 Andreas Veithen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import static com.google.common.truth.Truth.assertThat;
+
+import org.eclipse.core.runtime.Platform;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+import com.github.veithen.cosmos.osgi.runtime.Runtime;
+import com.github.veithen.cosmos.osgi.runtime.logging.simple.SimpleLogger;
+
+public class EclipseCoreRuntimeTest {
+    @Test
+    public void test() throws Exception {
+        Bundle bundle = Runtime.getInstance(SimpleLogger.INSTANCE).getBundle("org.eclipse.core.runtime");
+        bundle.start();
+        assertThat(Platform.isRunning()).isTrue();
+    }
+}
