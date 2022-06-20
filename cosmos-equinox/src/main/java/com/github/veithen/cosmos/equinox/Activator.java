c4f3574466a19727416e6f2a5761f4db76f75bec
Andreas Veithen <andreas.veithen@gmail.com>
2022-06-20 23:58:31 +0000

    Register the instance area with an "url" property
    Location services should be registered with an "url" property. Do this
    for the (temporary) instance area (aka Eclipse workspace). Note that
    this means that we can no longer create the corresponding directory on
    demand. Introduce a framework property to control the creation of the
    temporary instance area, and update the version number to reflect that
    incompatible change.

--- cosmos-equinox/src/main/java/com/github/veithen/cosmos/equinox/Activator.java
+++ cosmos-equinox/src/main/java/com/github/veithen/cosmos/equinox/Activator.java
@@ -19,21 +19,96 @@
  */
 package com.github.veithen.cosmos.equinox;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.osgi.signedcontent.SignedContentFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.github.veithen.cosmos.equinox.datalocation.LocationImpl;
 import com.github.veithen.cosmos.equinox.signedcontent.DummySignedContentFactory;
 
 public class Activator implements BundleActivator {
+    private static final Logger logger = LoggerFactory.getLogger(Activator.class);
+
+    // Instantiate this eagerly so that running the shutdown hook doesn't trigger
+    // NoClassDefFoundError if the class loader is closed.
+    private static final FileVisitor<Path> deletingFileVisitor =
+            new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+                        throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            };
+
+    private Path instanceArea;
+    private Thread shutdownHook;
+
     @Override
     public void start(BundleContext context) throws Exception {
         if ("true".equals(context.getProperty("cosmos.equinox.disableSignatureValidation"))) {
             context.registerService(
                     SignedContentFactory.class, new DummySignedContentFactory(), null);
         }
+        if ("true".equals(context.getProperty("cosmos.equinox.createTempInstanceArea"))) {
+            try {
+                instanceArea = Files.createTempDirectory("osgi.instance");
+                shutdownHook =
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                deleteInstanceArea();
+                            }
+                        };
+                Runtime.getRuntime().addShutdownHook(shutdownHook);
+                LocationImpl location = new LocationImpl(null, null, false);
+                URL url = instanceArea.toUri().toURL();
+                location.set(url, false);
+                Dictionary<String, Object> properties = new Hashtable<>();
+                properties.put(Location.SERVICE_PROPERTY_TYPE, Location.INSTANCE_AREA_TYPE);
+                properties.put(Location.SERVICE_PROPERTY_URL, url.toExternalForm());
+                context.registerService(Location.class, location, properties);
+            } catch (IOException ex) {
+                throw new RuntimeException(
+                        "Unable to create temporary directory for instance data", ex);
+            }
+        }
+    }
+
+    private void deleteInstanceArea() {
+        try {
+            Files.walkFileTree(instanceArea, deletingFileVisitor);
+        } catch (IOException ex) {
+            logger.error(String.format("Failed to delete directory %s", instanceArea), ex);
+        }
     }
 
     @Override
-    public void stop(BundleContext context) throws Exception {}
+    public void stop(BundleContext context) throws Exception {
+        if (instanceArea != null) {
+            deleteInstanceArea();
+            Runtime.getRuntime().removeShutdownHook(shutdownHook);
+        }
+    }
 }
--- cosmos-equinox/src/main/java/com/github/veithen/cosmos/equinox/datalocation/LocationImpl.java
+++ cosmos-equinox/src/main/java/com/github/veithen/cosmos/equinox/datalocation/LocationImpl.java
@@ -19,20 +19,109 @@
  */
 package com.github.veithen.cosmos.equinox.datalocation;
 
+import java.io.IOException;
 import java.net.URL;
 
 import org.eclipse.osgi.service.datalocation.Location;
 
-final class LocationImpl extends AbstractLocation {
+public final class LocationImpl implements Location {
+    private final Location parent;
     private final URL defaultValue;
+    private final boolean readOnly;
+    private URL location;
 
-    LocationImpl(Location parent, URL defaultValue, boolean readOnly) {
-        super(parent, readOnly);
+    public LocationImpl(Location parent, URL defaultValue, boolean readOnly) {
+        this.parent = parent;
         this.defaultValue = defaultValue;
+        this.readOnly = readOnly;
+    }
+
+    @Override
+    public boolean allowsDefault() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public URL getDefault() {
         return defaultValue;
+    }
+
+    @Override
+    public Location getParentLocation() {
+        return parent;
+    }
+
+    @Override
+    public synchronized URL getURL() {
+        if (location == null) {
+            URL defaultLocation = getDefault();
+            if (defaultLocation != null) {
+                setURL(defaultLocation, false);
+            }
+        }
+        return location;
+    }
+
+    @Override
+    public synchronized boolean isSet() {
+        return location != null;
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    @Override
+    public boolean setURL(URL value, boolean lock) throws IllegalStateException {
+        try {
+            return set(value, lock);
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public synchronized boolean set(URL value, boolean lock)
+            throws IllegalStateException, IOException {
+        if (lock) {
+            throw new UnsupportedOperationException();
+        }
+        if (location != null) {
+            throw new IllegalStateException();
+        }
+        location = value;
+        return true;
+    }
+
+    @Override
+    public boolean set(URL value, boolean lock, String lockFilePath)
+            throws IllegalStateException, IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean lock() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void release() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isLocked() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Location createLocation(Location parent, URL defaultValue, boolean readonly) {
+        return new LocationImpl(parent, defaultValue, readonly);
+    }
+
+    @Override
+    public URL getDataArea(String path) throws IOException {
+        throw new UnsupportedOperationException();
     }
 }
